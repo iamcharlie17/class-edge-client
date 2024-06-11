d4838a99d71594115aee9b96429289c7bf09b223
Riyad Hosen <riyadhosen@Riyads-Air.netis.cc>
2024-06-11 03:06:18 -0700

    added sponsor section

--- src/Pages/HomePage/Home/Home.jsx
+++ src/Pages/HomePage/Home/Home.jsx
@@ -1,14 +1,14 @@
-import useAuth from "../../../Hooks/useAuth";
+
 import Banner from "../Banner/Banner";
 import Categories from "../Categroy/Categories";
+import Sponsor from "../Sponsor/Sponsor";
 
 const Home = () => {
-    const {user} = useAuth()
-    console.log(user)
   return (
     <div className="min-h-[calc(100vh-240px)]">
       <Banner />
       <div className="mx-2 md:mx-16 lg:mx-24">
+        <Sponsor/>
         <Categories />
       </div>
     </div>
